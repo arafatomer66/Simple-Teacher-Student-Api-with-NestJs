19ef15cbb74ccfd148024347d2f599f0afaf317f
arafatomer66 <you@example.com>
2021-09-13 22:56:11 +0600

    getting request object

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,5 @@
-import { Controller , Get , Param , Put } from "@nestjs/common";
+import { Controller , Get , Param , Put , Post, Body } from "@nestjs/common";
+import { json } from "stream/consumers";
 
 @Controller('students')
 export class StudentController {
@@ -10,6 +11,12 @@ export class StudentController {
     @Get(':studentId')
     getStudentById(@Param('studentId') id: string) {
         return { 'your id is' : `${id}` } ;
+    }
+
+    @Post()
+    createStudent(@Body() body) {
+        console.log(body);
+        return { "body" : JSON.stringify(body) } ;
     }
 
     @Put(':studentId')
